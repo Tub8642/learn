fc4d335add41728c923175f6e85a5edf5b959b8c
anton212 <antonmiro222@gmail.com>
2026-05-21 18:56:41 +0300

    первая работающая страницач

--- video/admin.py
+++ video/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Project
+#from .models import Category, Project
 
-admin.site.register(Category)
-admin.site.register(Project)
+#admin.site.register(Category)
+#admin.site.register(Project)
--- video/models.py
+++ video/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 from django.utils.text import slugify
 
-class Category
-
+class Category(models.Model):
+    name = models.CharField(max_length=200, verbose_name='')
 
 class Video(models.Model):
     name = models.CharField(max_length=200, verbose_name='') #nazvanie
     pub_date = models.DateTimeField('date published') #data publikacii
     video_url = models.URLField() #ssylka na video
-    oblozhka_url = ???
+    oblozhka_url = models.URLField()
+
+class Meta:
+    ordering = ['category', '-published']
+
+def _str__(self):
+    return self.title

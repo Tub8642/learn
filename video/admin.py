69022d0e5684a50ec6515d00d9e58a3bad31aee4
anton212 <antonmiro222@gmail.com>
2026-06-10 16:03:21 +0300

    Довёл модели до ума и добавил админку

--- video/admin.py
+++ video/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-#from .models import Category, Project
-
-#admin.site.register(Category)
-#admin.site.register(Project)
+from .models import Category, Project, ContactMessage
+admin.site.register(Category)
+admin.site.register(Project)
+admin.site.register(ContactMessage)
--- video/models.py
+++ video/models.py
@@ -3,6 +3,14 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
@@ -25,14 +33,22 @@ class Project(models.Model):
         verbose_name = 'Проект'
         verbose_name_plural = 'Проекты'
 
-    class ContactMessage(models.Model):
-        CONTACT_CHOISES = [
-            ('email', 'Email'),
-            ('phone', 'Телефон')
-            ('vk', 'Вконтакте')
-        ]
-        name = models.CharField(max_length=100)
-        contact_type = models.CharField(max_length=50, choices=[CONTACT_CHOISES])
-        contact_value = models.CharField(max_length=200)
-        massage = models.TextField()
-        send_at = models.DateField(auto_now_add=True)
+class ContactMessage(models.Model):
+    CONTACT_CHOISES = [
+        ('email', 'Email'),
+        ('phone', 'Телефон'),
+        ('vk', 'Вконтакте'),
+    ]
+    name = models.CharField(max_length=100)
+    contact_type = models.CharField(max_length=50, choices=CONTACT_CHOISES)
+    contact_value = models.CharField(max_length=200)
+    massage = models.TextField()
+    sent_at = models.DateField(auto_now_add=True)  
+
+    def __str__(self):
+        return f'Сообщение от {self.name}'
+    
+    class Meta:
+        ordering = ['sent_at']
+        verbose_name = 'контакт'
+        verbose_name_plural = 'контакты'
